3d68ef11f6463120811a141bb5121d3c34ab433d
mt5225 <mt5225@gmail.com>
2016-02-04 22:32:38 -0800

    add socketio

---
+++ packs/lab/actions/socketio_post.py
@@ -0,0 +1,22 @@
+from socketIO_client import SocketIO, LoggingNamespace
+from st2actions.runners.pythonrunner import Action
+
+
+class PostMessageAction(Action):
+    def __init__(self, config):
+        super(PostMessageAction, self).__init__(config=config)
+
+    def run(self, **kwargs):
+        self.logger.info(kwargs)
+
+        # get socketio server info from configuration
+        config = self.config['socketio']
+
+        # get message from parameter
+        message = kwargs['socketio_message']
+
+        #send message to socketio server
+        with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
+            socketIO.emit(config['topic'], message)
+
+        return
