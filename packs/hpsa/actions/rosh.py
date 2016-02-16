f9b9a704f36720434fb433b2086afe33d75bdd38
mt5225 <mt5225@gmail.com>
2016-02-16 11:04:33 -0800

    updated

--- packs/hpsa/actions/rosh.py
+++ packs/hpsa/actions/rosh.py
@@ -21,7 +21,7 @@ class RoshAction(Action):
             connect_timeout=int(kwargs['connection_timeout']),
             missing_host_key=spur.ssh.MissingHostKey.accept
         )
-        command = "echo rosh -n %s -l %s %s" % (kwargs['target_hostname'], kwargs['run_as_username'], kwargs['run_command'])
+        command = "rosh -n %s -l %s %s" % (kwargs['target_hostname'], kwargs['run_as_username'], kwargs['run_command'])
         result = {}
         with shell:
             try:
